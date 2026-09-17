bb4c34918c12fce1912047e2552810f1ee0438fb
Rahul Bastia <151760382+rahulbastia00@users.noreply.github.com>
2026-09-17 07:46:48 +0000

    created viws and urls

--- messaging/views.py
+++ messaging/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Channel, Message
+from .serializers import ChannelSerializer, MessageSerializer
 
-# Create your views here.
+class ChannelListCreateView(generics.ListCreateAPIView):
+    queryset = Channel.objects.all()
+    serializer_class = ChannelSerializer
+
+    def perform_create(self, serializer):
+        channel = serializer.save()
+        channel.members.add(self.request.user)
+
+class MessageListCreateView(generics.ListCreateAPIView):
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        channel_id = self.kwargs['channel_id']
+        return Message.objects.filter(channel_id=channel_id)
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
--- syncegine_core/urls.py
+++ syncegine_core/urls.py
@@ -1,23 +1,14 @@
-"""
-URL configuration for syncegine_core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from messaging.views import ChannelListCreateView, MessageListCreateView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    # JWT Authentication Endpoints
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # REST Messaging Endpoints
+    path('api/channels/', ChannelListCreateView.as_view(), name='channel-list'),
+    path('api/channels/<int:channel_id>/messages/', MessageListCreateView.as_view(), name='channel-messages'),
 ]
